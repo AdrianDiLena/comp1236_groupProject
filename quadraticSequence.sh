cfe68553eaf42f4905bbf362a2390ea15b7c6a98
Adrian <adrian.dilena@gmail.com>
2025-11-24 18:18:06 -0500

    changed const

--- quadraticSequence.sh
+++ quadraticSequence.sh
@@ -3,9 +3,9 @@
 echo "Hello!"
 echo "Welcome to the quadratic sequencing portion of tonight's application. Please enjoy."
 echo "Please enter 3 non-negative numbers:"
-read con_a
-read con_b
-read con_c
+read const_a
+read const_b
+read const_c
 echo "Thank you, we're almost ready."
 echo "Please enter two numbers for the range of the sequence."
 read range_1
@@ -18,7 +18,7 @@ for (( n=range_1; n<=range_2; n++ )); do        # For loop using user input rang
     echo "Term Sum is: "$term_sum               # State the sum of all terms each loop cycle
     let term=$((con_a*n**2+con_b*n+con_c))      # Calculate the terms from using the Quadratic Sequence equation 
     echo "Term is "$term                        # Print the term/result of the equation 
-    
+
     term_sum=$(( term_sum + term ))             # Add the most recent term to the variable representing the sum of quadratic terms  
 done
 echo "The sum of Terms: "$term_sum
