86687a93b857fab94d996193730fd9681e11b545
Adrian <adrian.dilena@gmail.com>
2025-11-25 11:08:57 -0500

    fixed a variable mismatch

--- quadraticSequence.sh
+++ quadraticSequence.sh
@@ -12,13 +12,12 @@ read range_1
 read range_2
 echo "Now please enter your banking PIN number... just kidding... brb."
 
-let term_sum=0
+term_sum=0
 
 for (( n=range_1; n<=range_2; n++ )); do        # For loop using user input range_1, range_2
     echo "Term Sum is: "$term_sum               # State the sum of all terms each loop cycle
-    let term=$((con_a*n**2+con_b*n+con_c))      # Calculate the terms from using the Quadratic Sequence equation 
+    term=$((const_a*n**2+const_b*n+const_c))    # Calculate the terms from using the Quadratic Sequence equation 
     echo "Term is "$term                        # Print the term/result of the equation 
-
     term_sum=$(( term_sum + term ))             # Add the most recent term to the variable representing the sum of quadratic terms  
 done
 echo "The sum of Terms: "$term_sum
