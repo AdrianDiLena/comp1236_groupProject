2ba600c9c4111ceb2cc521969967468b4d6e62f8
Adrian <adrian.dilena@gmail.com>
2025-12-05 23:27:12 -0500

    okokok... everything is in main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,7 @@
-#password
+#!/bin/bash
+
+# Password Function
+
 PASSWORD="w22"
 attempts=0
 
@@ -27,6 +30,133 @@ password() {
     fi
 }
 
+# Square Numbers Function
+
+SquareNum_Function(){
+
+    read -p "Pick a number: " a
+    read -p "How long will the list be pick a number: " list
+    read -p "sum of two consecutive square maximum value: " value
+
+    #odd position code
+
+    a=$a
+    x=0
+
+    while [ $x -lt $list ]
+    do
+        echo "$a^2 = $((a * a))"
+        a=$((a + 2))
+        x=$((x + 1))
+    done
+
+    # two consecutive squares
+
+    y=1
+    total=0
+
+    while :
+    do
+        sum=$((y*y + (y+1)*(y+1)))
+
+        if [ $sum -gt $value ]; then
+            break
+        fi
+
+        echo "$y^2 + $((y+1))^2 = $sum"
+
+        total=$((total + 1))
+        y=$((y + 1))
+    done
+
+    echo "Total odd numbers: $total"
+    }
+
+# Quadratic Sequence Function
+
+quad_seq_function() {
+    echo "Hello!"
+    echo
+    echo "Welcome to the quadratic sequencing portion of tonight's application. Please enjoy."
+    echo "Please enter 3 non-negative numbers:"
+    echo
+    read const_a
+    read const_b
+    read const_c
+    echo
+    echo "Thank you, we're almost ready."
+    echo "Please enter two numbers for the range of the sequence."
+    echo
+    read range_1
+    read range_2
+    echo
+    echo "######################################################################"
+    echo
+    term_sum=0
+
+    for (( n=range_1; n<=range_2; n++ )); do        
+        term=$((const_a*n**2+const_b*n+const_c))
+        if [ $n -eq $range_1 ]; then
+            first_term=$term
+        fi    
+
+        is_prime=0
+        
+        if (( term<=1 )); then
+            is_prime=1
+        fi
+
+        i=2
+
+        if (( is_prime == 0 )); then
+                while (( i < term )); do
+                    if (( term % i == 0 )); then
+                        is_prime=1 
+                        break 
+                    fi
+                    
+                    i=$(( i + 1 ))
+                done
+            fi
+        
+            if (( is_prime == 0 )); then
+                echo $term " -> This term IS a prime number."
+            else
+                echo $term " -  This term is NOT a prime number."
+            fi
+
+        term_sum=$(( term_sum + term ))
+    done
+
+    echo "The sum total of Terms: "$term_sum
+    echo
+    echo "######################################################################"
+    echo
+
+    product_term=$(( first_term*term ))
+
+    echo "The product of the first term ("$first_term") and last term ("$term") is: "$product_term
+    echo 
+    echo "Choose a number:"
+    read product_num
+    echo
+
+    if (( (first_term * term) % product_num == 0 )); then
+        echo "The product of the first and last terms is divisible by "$product_num"!!!"
+    else 
+        echo "The product of the first and last terms is not divisible by" $product_num ", sorry."
+    fi
+
+    echo
+    echo "######################################################################"
+    echo
+    echo "And that concludes the quadratic sequence portion of this application."
+    echo "Have a nice day."
+
+}
+
+# Main Menu Function
+
 mainMenu() {
 
     # FIXED: Added 'do' after the semicolon
@@ -54,10 +184,10 @@ mainMenu() {
                 Task1
                 ;;
             S|s)
-                Task2
+                SquareNum_Function
                 ;;
             Q|q)
-                Task3
+                quad_seq_function
                 ;;
             EXIT|exit)
                 echo "Bye Bye"
